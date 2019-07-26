2bf47e786050703010e90a273f32a3666f53155d
Andrey Pribytkov <abprib@mail.ru>
2019-07-26 23:03:06 +0500

    Update HashMaps_hackerrank.java

--- HashMaps_hackerrank.java
+++ HashMaps_hackerrank.java
@@ -1,31 +1,32 @@
 import java.util.*;
-import java.io.*;
 
-class Main {
-  public static void main(String[] args) {
+class Panel {
+    public static void main(String[] args) {
 
-    Scanner sc = new Scanner(System.in);
-    System.out.print("Enter the amount of words in the phoneBook: ");
-    int amount = sc.nextInt(); 
-    HashMap<String, Integer> phoneBook = new HashMap<String, Integer>();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the amount of words in the phoneBook: ");
+        int amount = sc.nextInt();
+        HashMap<String, Integer> phoneBook = new HashMap<String, Integer>();
+        String name;
+        int phone;
 
-    for(int i = 0; i < amount; i++){
-      System.out.print("Enter the name: ");
-      String name = sc.nextLine();
-      System.out.print("\b Enter the number: ");
-      int phone = sc.nextInt();
-      phoneBook.put(name, phone);
+        for(int i = 0; i < amount; i++){
+            System.out.print("Enter the name: ");
+            name = sc.next();
+            System.out.print("Enter the number: ");
+            phone = sc.nextInt();
+            phoneBook.put(name, phone);
+        }
+
+        System.out.print("Enter name, I give you number: ");
+        String target = sc.next();
+        if(phoneBook.containsKey(target)){
+            System.out.println(target + " : " + phoneBook.get(target));
+        } else {
+            System.out.println("There is no such a contact");
+        }
+        sc.close();
     }
 
-    while(sc.hasNext()){
-      String target = sc.nextLine();
-      if(phoneBook.containsKey(target)){
-        System.out.println(target + " : " + phoneBook.get(target));
-      } else {
-        System.out.println("There is no such a contact");
-      }
-    }
-    sc.close();
 
-  }
 }
